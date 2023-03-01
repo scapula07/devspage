36b568ef143665927a2710b4bd38bceaab339a88
scapula07 <bartholomewonogwu@yahoo.com>
2023-03-01 02:38:23 +0100

    fix to bigint

--- config-overrides.js
+++ config-overrides.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
     Object.assign(fallback, {
-        "crypto": require.resolve("crypto-browserify"),
+        // "crypto": require.resolve("crypto-browserify"),
         "stream": require.resolve("stream-browserify"),
         "assert": require.resolve("assert"),
         "http": require.resolve("stream-http"),
